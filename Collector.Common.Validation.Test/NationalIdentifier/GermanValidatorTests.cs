9eee863b40ce5de50e4db6a725e62ed952b597fb
tapmui <tapio.muikku@leanware.fi>
2021-03-11 08:20:18 +0200

    german birthday validation support

---
+++ Collector.Common.Validation.Test/NationalIdentifier/GermanValidatorTests.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections;
+using Collector.Common.Validation.NationalIdentifier;
+using Collector.Common.Validation.NationalIdentifier.Validators;
+using NUnit.Framework;
+
+namespace Collector.Common.Validation.Test.NationalIdentifier
+{
+    public class GermanValidatorTests
+    {
+        private DateTimeOffset                     _defaultTestTime;
+        private GermanyNationalBirthdayIdentifierValidator _sut;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _sut             = new GermanyNationalBirthdayIdentifierValidator();
+            _defaultTestTime = new DateTimeOffset(new DateTime(2010, 01, 01), TimeSpan.Zero);
+        }
+
+        [TestCaseSource(typeof(GermanIdentifiers), nameof(GermanIdentifiers.Valid))]
+        public void IsValid_NationalIdentifierIsValid_ReturnTrue(string nationalIdentifier)
+        {
+            SystemTime.TestTime = _defaultTestTime;
+
+            var actual = _sut.IsValid(nationalIdentifier);
+            Assert.That(actual, Is.True);
+            var parsedResult = _sut.Parse(nationalIdentifier);
+            Assert.That(parsedResult.Valid, Is.True);
+        }
+
+        [TestCaseSource(typeof(GermanIdentifiers), nameof(GermanIdentifiers.Invalid))]
+        public void IsValid_NationalIdentifierIsInvalid_ReturnFalse(string nationalIdentifier)
+        {
+            SystemTime.TestTime = _defaultTestTime;
+
+            var actual = _sut.IsValid(nationalIdentifier);
+            Assert.That(actual, Is.False);
+            var parsedResult = _sut.Parse(nationalIdentifier);
+            Assert.That(parsedResult.Valid, Is.False);
+        }
+        
+        [TestCaseSource(typeof(GermanIdentifiers), nameof(GermanIdentifiers.Normalized))]
+        public void Normalize_NationalIdentifierIsValid_ReturnNormalizedValue(
+            string nationalIdentifier, string expectedValue)
+        {
+            SystemTime.TestTime = _defaultTestTime;
+
+            var actual = _sut.Normalize(nationalIdentifier);
+
+            Assert.That(actual, Is.EqualTo(expectedValue));
+        }
+
+
+        [TestCaseSource(typeof(GermanIdentifiers), nameof(GermanIdentifiers.Invalid))]
+        public void Normalize_NationalIdentifierIsInvalid_ThrowArgumentException(string nationalIdentifier)
+        {
+            SystemTime.TestTime = _defaultTestTime;
+
+            var ex = Assert.Throws<ArgumentException>(() => _sut.Normalize(nationalIdentifier));
+            Assert.That(ex.ParamName, Is.EqualTo("nationalIdentifier"), nameof(ex.ParamName));
+            Assert.That(ex.Message,
+                Does.Contain(nationalIdentifier ?? string.Empty).And.Contain(_sut.CountryCode.ToString()),
+                nameof(ex.Message));
+        }
+
+        public class GermanIdentifiers
+        {
+            public static IEnumerable Valid
+            {
+                get
+                {
+                    yield return "01.01.2010";
+                    yield return "31.01.2010";
+                    yield return "01.12.2010";
+                    yield return "01.01.2010";
+                    yield return "31.12.2010";
+                    yield return "29.02.1932"; // 1932 has leapyear
+                    yield return "29.02.2004";   // 2004 has leapyear
+                }
+            }
+
+            public static IEnumerable Invalid
+            {
+                get
+                {
+                    yield return null;
+                    yield return "xx";          // Invalid
+                    yield return "311280"; // Invalid Format
+                    yield return "33.00.1971"; // Invalid month
+                    yield return "33.13.1919"; // Invalid month
+                    yield return "33.13.19191"; // Invalid year
+                    yield return "33.01.1949"; // Invalid day
+                    yield return "00.01.1959"; // Invalid day
+                    yield return "29.02.1933"; // 1933 does not have leapyear
+                    yield return "29.02.2005"; // 2005 does not have leapyear
+                    yield return "01.01,2010"; // Invalid Format
+                }
+            }
+
+            public static IEnumerable Normalized
+            {
+                get
+                {
+                    yield return new TestCaseData("18.10.1964", "19641018");
+                    yield return new TestCaseData("01.01.1964", "19640101");
+                    yield return new TestCaseData("20.05.1980", "19800520");
+                    yield return new TestCaseData("29.02.2004", "20040229"); // 2004 has leapyear
+                }
+            }
+        }
+    }
+}
---
+++ Nationaldentifier/Validators/GermanyNationalBirthdayIdentifierValidator.cs
@@ -0,0 +1,79 @@
+﻿using Collector.Common.Validation.NationalIdentifier.Interface;
+using System;
+using System.Text.RegularExpressions;
+
+namespace Collector.Common.Validation.NationalIdentifier.Validators
+{
+    /// <remarks>
+    /// Allowed formats: DD.MM.YYYY
+    ///    where DD = day, MM = month, YY = year
+    /// </remarks>
+    public class GermanyNationalBirthdayIdentifierValidator : NationalIdentifierValidator
+    {
+        /// <remarks>Regex has been check with SDL Regex Fuzzer tool so it should not be a source for DOS attacks. IF YOU UPDATE THE REGEX recheck it again with the SDL tool.</remarks>>
+        private static readonly Regex NationalIdentifierWhitelistValidator =
+            new Regex(@"^(\d{2}\.\d{2}\.\d{4})$", RegexOptions.ECMAScript);
+
+
+        public override CountryCode CountryCode => CountryCode.DE;
+
+        public override bool IsValid(string nationalIdentifier)
+        {
+            if (nationalIdentifier == null)
+                return false;
+            
+            if (!NationalIdentifierWhitelistValidator.IsMatch(nationalIdentifier))
+            {
+                return false;
+            }
+
+                var valueToCheck = nationalIdentifier;
+            // valueToCheck should contain the following format DD.MM.YYYY
+            var day = int.Parse(valueToCheck.Substring(0, 2));
+            var month = int.Parse(valueToCheck.Substring(3, 2));
+            var yearhWithCentury = int.Parse(valueToCheck.Substring(6, 4));
+
+            return IsValidDate(yearhWithCentury, month, day);
+        }
+
+		public override ParsedNationalIdentifierData Parse(string nationalIdentifier)
+		{
+			ParsedNationalIdentifierData parsedObj = new ParsedNationalIdentifierData();
+            parsedObj.Valid = IsValid(nationalIdentifier);
+
+            if (parsedObj.Valid)
+			{
+				
+                parsedObj.Gender = Gender.UNKNOWN;
+                var valueToCheck = nationalIdentifier;
+                // valueToCheck should contain the following format DD.MM.YYYY
+                var day = int.Parse(valueToCheck.Substring(0, 2));
+                var month = int.Parse(valueToCheck.Substring(3, 2));
+                var yearhWithCentury = int.Parse(valueToCheck.Substring(6, 4));
+                var birthday = new DateTime(yearhWithCentury, month, day);
+				parsedObj.DateOfBirth = birthday;
+				parsedObj.AgeInYears = GetAge(birthday);
+			}
+			
+			return parsedObj;
+		}
+
+		/// <summary>Transforms valid national identifier into format YYYYMMDD.</summary>
+		/// <exception cref="ArgumentException">If <paramref name="nationalIdentifier"/> is not valid.</exception>
+		public override string Normalize(string nationalIdentifier)
+        {
+            if (!IsValid(nationalIdentifier))
+                throw new ArgumentException(
+                    ErrorMessages.GetInvalidIdentifierMessage(nationalIdentifier, CountryCode),
+                    nameof(nationalIdentifier));
+
+            var valueToCheck = nationalIdentifier;
+            // valueToCheck should contain the following format DD.MM.YYYY
+            var day = int.Parse(valueToCheck.Substring(0, 2));
+            var month = int.Parse(valueToCheck.Substring(3, 2));
+            var yearhWithCentury = int.Parse(valueToCheck.Substring(6, 4));
+            return yearhWithCentury.ToString() + month.ToString("00") + day.ToString("00");
+        }
+
+    }
+}
